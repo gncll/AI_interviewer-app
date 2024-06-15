e1ca8f784d89b08284d4d539389225bf2eae6907
Gencay <bilinclizihin@gmail.com>
2024-06-15 14:12:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if "start_chat" not in st.session_state:
 if "thread_id" not in st.session_state:
     st.session_state.thread_id = None
 
-st.set_page_config(page_title="CatGPT", page_icon=":speech_balloon:")
+st.set_page_config(page_title="Interview Assistant", page_icon=":speech_balloon:")
 
 if st.sidebar.button("Start Chat"):
     st.session_state.start_chat = True
